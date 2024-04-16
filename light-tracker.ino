93d73bb96abe071c981774ecb50b5a9995c02202
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2024-04-16 10:33:53 +0200

    Define a basic idea for the servo controller

--- light-tracker.ino
+++ light-tracker.ino
@@ -1,9 +1,12 @@
 #include <Servo.h>
+#include <math.h>
 
 Servo myservo;  // create servo object to control a servo
 
 int servoPin = 3;
 int sensorPin = PIN_A0;
+
+int previousLightLevel; // Variable to track the previous read's light levels. used for delta calculations
 
 void setup() {
     myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
@@ -15,6 +18,14 @@ void setup() {
 void loop() {
     // Read the value from the photoresistor:
     int lightLevel = analogRead(sensorPin);
+
+    // calculate difference in light level since last scan
+    int deltaLight = lightLevel - previousLightLevel;
+
+    // if the absolute difference is large enough, then we should move. otherwise, stand still
+    if ( abs(deltaLight) > 10) {
+        // rotate servo
+    }
 
     // Print the light level to the serial monitor:
     Serial.println(lightLevel);
