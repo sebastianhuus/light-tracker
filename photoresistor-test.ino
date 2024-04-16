2d9526629fcbf5d32afa8d843f1879eab663ffee
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2024-04-16 16:13:06 +0200

    documentation

--- photoresistor-test.ino
+++ photoresistor-test.ino
@@ -1,10 +1,23 @@
+
+
+/**
+ * @file photoresistor-test.ino
+ * @brief This file contains functions to test a photoresistor and print the light level to the serial monitor.
+ */
+
 int sensorTestPin = PIN_A0;
 
+/**
+ * @brief Initializes the photoresistor by setting up the serial communication.
+ */
 void initPhotoresistor() {
     // Initialize serial communication at 9600 bits per second:
     Serial.begin(9600);
 }
 
+/**
+ * @brief Tests the photoresistor by reading the light level and printing it to the serial monitor.
+ */
 void testPhotoresistor() {
     // Read the value from the photoresistor:
     int testLightLevel = analogRead(sensorTestPin);
