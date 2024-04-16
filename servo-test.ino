d0aefc1f180d1f77fa783a7343f2d6f092be9ff5
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2024-04-16 10:25:23 +0200

    REname test variables to be unique. otherwise can't build

--- servo-test.ino
+++ servo-test.ino
@@ -1,21 +1,20 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myTestservo;  // create servo object to control a servo
 
-int servoPin = 3;
-int sensorPin = PIN_A0;
+int servoTestPin = 3;
 
-void setup() {
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+void initServo() {
+  myTestservo.attach(servoTestPin);  // attaches the servo on pin 9 to the servo object
 }
 
-void loop() {
+void testServo() {
   for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myTestservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
   for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees back to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myTestservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
