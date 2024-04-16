47aa0d903873b2db2d84a276667242f02edf423c
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2024-04-16 16:14:18 +0200

    add more docs

--- servo-test.ino
+++ servo-test.ino
@@ -1,3 +1,8 @@
+/*
+  This file contains code to control a servo motor using an Arduino board.
+  The servo motor rotates from 0 degrees to 180 degrees and then back to 0 degrees.
+*/
+
 #include <Servo.h>
 
 Servo myTestservo;  // create servo object to control a servo
@@ -5,10 +10,16 @@ Servo myTestservo;  // create servo object to control a servo
 int servoTestPin = 3;
 
 void initServo() {
+  /*
+    Initializes the servo by attaching it to the specified pin.
+  */
   myTestservo.attach(servoTestPin);  // attaches the servo on pin 9 to the servo object
 }
 
 void testServo() {
+  /*
+    Tests the servo by rotating it from 0 degrees to 180 degrees and then back to 0 degrees.
+  */
   for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     myTestservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
