6e6b6ae7eb4f9a74131cfe49782ed277190c6130
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2024-04-16 10:25:44 +0200

    implement a basic light sensor reader

--- light-tracker.ino
+++ light-tracker.ino
@@ -6,16 +6,19 @@ int servoPin = 3;
 int sensorPin = PIN_A0;
 
 void setup() {
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+    myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+
+    // Initialize serial communication at 9600 bits per second:
+    Serial.begin(9600);
 }
 
 void loop() {
-  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees back to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+    // Read the value from the photoresistor:
+    int lightLevel = analogRead(sensorPin);
+
+    // Print the light level to the serial monitor:
+    Serial.println(lightLevel);
+
+    // Wait for a short period to avoid spamming the serial monitor:
+    delay(250);
 }
